a56bff53c44f237c6e051e13ace6e713d3a38f70
Selin Huang <selin.hyx@gmail.com>
2019-07-08 01:43:26 +0000

    implemented accounts and transaction objects

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
-let balance = 500.00;
+// let balance = 500.00;
 
-class Withdrawal {
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-  constructor(amount) {
+  get balance() {
+    let balance = 0;
+    for(let t of this.transactions) {
+      balance += t.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
-
   commit() {
-    balance -= this.amount;
+    if (!this.isAllowed()) return false;
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
+  }
+}
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
   }
 
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+}
+
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+
+  isAllowed() {
+    return true;
+  }
 }
 
 
@@ -17,13 +56,23 @@ class Withdrawal {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("totoro");
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+console.log("inital balance", myAccount.balance);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+t1 = new Withdrawal(50.25, myAccount);
+console.log("initial commit result:", t1.commit());
 
-console.log('Balance:', balance);
+
+t3 = new Deposit(120.00, myAccount);
+console.log("commit result of +120:", t3.commit());
+
+t2 = new Withdrawal(9.99, myAccount);
+console.log("commit result of -9.99:", t2.commit());
+
+// console.log("balance of total withdrawal:", myAccount.balance)
+
+console.log(`final lance:`, myAccount.balance);
+
+console.log("transaction history:", myAccount.transactions);
+
